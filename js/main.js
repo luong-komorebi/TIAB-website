7c9275977e0ccc9c36ffa0f58fb66301fc895451
LuongVo <vo.tran.thanh.luong@gmail.com>
2016-09-04 21:57:26 +0700

    Added Jquerry effects, finializing html css

---
+++ js/main.js
@@ -0,0 +1,63 @@
+$(document).ready(function () {
+	"use-strict";
+
+	// Scrolling page feature - require Easing plugin
+	$('a.page-scroll').bind('click', function(event) {
+		var $anchor = $(this);
+		$('html, body').stop().animate({
+			scrollTop: ($($anchor.attr('href')).offset().top)
+		}, 1250, 'easeInOutExpo');
+		event.preventDefault();
+	});
+
+	// Highlight the top nav as scrolling occurs
+	$('body').scrollspy({
+		target: '.navbar-fixed-top',
+		offset: 51 // Specifies the number of pixels to offset from top when calculating the position of scroll
+	})
+
+	// Closes the Responsive Menu on Menu Item Click
+    $('.navbar-collapse ul li a').click(function() {
+        $('.navbar-toggle:visible').click();
+    });
+
+	//Offset for main navigation 
+	$('#mainNav').affix({
+		offset: {
+			top: 100,
+		}
+	})
+	window.sr = ScrollReveal();
+    sr.reveal('.sr-icons', {
+        duration: 600,
+        scale: 0.2,
+        distance: '0px'
+    }, 200);
+    sr.reveal('.sr-button', {
+        duration: 1000,
+        delay: 200
+    });
+    sr.reveal('.sr-contact', {
+        duration: 600,
+        scale: 0.3,
+        distance: '0px'
+    }, 300);
+
+    // Initialize and Configure Magnific Popup Lightbox Plugin
+    $('.popup-gallery').magnificPopup({
+        delegate: 'a',
+        type: 'image',
+        tLoading: 'Loading image #%curr%...',
+        mainClass: 'mfp-img-mobile',
+        gallery: {
+            enabled: true,
+            navigateByImgClick: true,
+            preload: [0, 1] // Will preload 0 - before current, and 1 after the current image
+        },
+        image: {
+            tError: '<a href="%url%">The image #%curr%</a> could not be loaded.'
+        }
+    });
+
+
+})
